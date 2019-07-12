fd3a8581fa64cf70403026435b58b71db01065ee
Danny Rooker <dannyrooks@gmail.com>
2019-07-12 07:41:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+
+def map(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length
+   new_array[i] = yield source_array[i]
+   i += 1
+  end
+   return new_array
+ end
+ 
+def reduce(source_array, starting_point = 0)
+ new_array = []
+ i = 0
+ yield source_array.map
+  while i < source_array.length do
+  new_array[i] = yield source_array[i]
+  i += 1
+  
+   return new_array
+end
